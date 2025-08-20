e8d79fc3f04ae63a2d539d5e19c72b79e63b1a19
SamehElkhyat <ssalih292@gmail.com>
2025-08-20 23:02:25 +0300

    first

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from '../styles/VideoBackground.module.css';
 
 const VideoBackground = () => {
   return (
     <div className={styles.videoContainer}>
-
-      <div className={styles.video} style={{
-        background: `url('https://cnn-arabic-images.cnn.io/cloudinary/image/upload/w_1920,c_scale,q_auto/cnnarabic/2023/09/18/images/249868.jpg') no-repeat center center`,
-        backgroundSize: 'cover'
-      }}></div>
-      
+      <div className={styles.imageWrapper}>
+        <Image 
+          src="/bisha-background.jpg" 
+          alt="Bisha Background"
+          fill
+          priority
+          quality={100}
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center'
+          }}
+        />
+      </div>
       <div className={styles.overlay}></div>
     </div>
   );
